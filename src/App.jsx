8b2c76884cb125781eab6155a82fa28b8fe2e8ea
EmPi9 <empi9@a25.ru>
2025-03-11 20:27:05 +0300

    add: Добавили навигацию

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css'
 import './index.css'
 import { PreLoaderScreen } from './components/PreLoaderScreen'
+import { Navbar } from "./components/Navbar"
+import { MobileMenu } from "./components/MobileMenu"
 import { useState } from 'react'
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
     
   return (
     <>
       {!isLoaded && <PreLoaderScreen onComplete={() => setIsLoaded(true)}/>}{" "}
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-        <Navbar></>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       </div>
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+export const Navbar = ( {menuOpen, setMenuOpen} ) => {
+
+    useEffect(() => {
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    }, [menuOpen])
+
+    return (<nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+        <div className="max-w-5xl mx-auto px-4">
+            <div className="flex justify-between items-center h-16">
+                <a href="#home" className="font-mono text-xl font-bold text-white">KH<span className="text-blue-600">.EmPi9</span></a>
+
+                <div className="w-7 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
+                    &#9776;
+                </div>
+
+                <div className="hidden md:flex items-center space-x-8">
+                    <a href="#home" className="text-gray-300 hover:text-white transition-colors">Главная</a>
+                    <a href="#about" className="text-gray-300 hover:text-white transition-colors">О себе</a>
+                    <a href="#projects" className="text-gray-300 hover:text-white transition-colors">Проекты</a>
+                    <a href="#contacts" className="text-gray-300 hover:text-white transition-colors">Контакты</a>
+                </div>
+            </div>
+        </div>
+    </nav>);
+}
